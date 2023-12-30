516f6578469888e0fdac8b3335352951dec426b0
AntonXCM <antony.prykhodchenko@gmail.com>
2023-12-30 20:55:12 +0200

    нинзя

---
+++ Assets/Scripts/Enemies/PinkCubeEnemy.cs
@@ -0,0 +1,45 @@
+using Unity.VisualScripting;
+
+using UnityEngine;
+
+public class PinkCubeEnemy : RedCubeEnemy
+{
+    
+    [SerializeField] float _attackRange;
+    Animator _anim;
+    protected override void Start()
+    {
+        base.Start();
+        _damage = 1;
+        _maxSpeed = 2;
+        _normalSpeed = 4;
+        _anim = GetComponent<Animator>();
+    }
+    protected override void Move()
+    {
+        if(Vector2.Distance(transform.position,target.position) >= _attackRange)
+        {
+            base.Move();
+        }
+    }
+    protected override void Attack()
+    {
+        if(Vector2.Distance(transform.position,target.position) < _attackRange)
+        {
+            _anim.Play("Attack");
+        }
+    }
+    public void AttackAnimCallback()
+    {
+        if(Vector2.Distance(transform.position,target.position) < _attackRange)
+        {
+            target.GetComponent<PlayerHealth>().ApplyDamage(_damage);
+        }
+    }
+    protected override void CloseAttack(Collision2D other)
+    {
+        _normalSpeed = _maxSpeed;
+        _anim.Play("Stun");
+    }
+    
+}
