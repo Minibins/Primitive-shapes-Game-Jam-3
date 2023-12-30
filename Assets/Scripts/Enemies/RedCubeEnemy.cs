b361b9d451699ec1b89645c0d4fe44c0f877424c
AntonXCM <antony.prykhodchenko@gmail.com>
2023-12-30 02:25:09 +0200

    Научил красного чела ходить

--- Assets/Scripts/Enemies/RedCubeEnemy.cs
+++ Assets/Scripts/Enemies/RedCubeEnemy.cs
@@ -1,7 +1,11 @@
+using Unity.VisualScripting;
+
 using UnityEngine;
 
 public class RedCubeEnemy : Enemy
 {
+    [SerializeField] LayerMask _wallMask;
+    private Vector2 lastPos;
     protected override void Start()
     {
         base.Start();
@@ -14,8 +18,22 @@ public class RedCubeEnemy : Enemy
     {
         Vector3 _direction = (target.position - transform.position).normalized;
         Vector3 _movement = _direction * _normalSpeed * Time.fixedDeltaTime;
-        
+        while(
+            Physics2D.Raycast(transform.position+new Vector3(0.5f,0.5f,0),_movement,_normalSpeed,_wallMask)||
+            Physics2D.Raycast(transform.position + new Vector3(0.5f,-0.5f,0),_movement,_normalSpeed,_wallMask) ||
+            Physics2D.Raycast(transform.position + new Vector3(-0.5f,-0.5f,0),_movement,_normalSpeed,_wallMask) ||
+            Physics2D.Raycast(transform.position + new Vector3(-0.5f,0.5f,0),_movement,_normalSpeed,_wallMask))
+        {
+            _movement = MathAVM.MathA.RotatedVector(_movement, 25);
+            if(_movement == _direction * _normalSpeed * Time.fixedDeltaTime) break;
+        }
+        if(lastPos == transform.position.ConvertTo<Vector2>())
+        {
+            _movement = MathAVM.MathA.RotatedVector(_movement,180);
+        }
+        Debug.DrawRay(transform.position,_movement*22);
         _rigidbody.MovePosition(transform.position + _movement);
+        lastPos = transform.position;
     }
 
     protected override void CloseAttack(Collision2D other)
