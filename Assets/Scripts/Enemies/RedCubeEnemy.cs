5b1a24c00160ba286edeffaf72f85a811649b74d
AntonXCM <antony.prykhodchenko@gmail.com>
2023-12-30 02:41:13 +0200

    Теперь красные челы умеют ходить ещё и влево Ü

--- Assets/Scripts/Enemies/RedCubeEnemy.cs
+++ Assets/Scripts/Enemies/RedCubeEnemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RedCubeEnemy : Enemy
 {
-    [SerializeField] LayerMask _wallMask;
+    [SerializeField] LayerMask _wallMask, _playerMask;
     private Vector2 lastPos;
     protected override void Start()
     {
@@ -18,22 +18,26 @@ public class RedCubeEnemy : Enemy
     {
         Vector3 _direction = (target.position - transform.position).normalized;
         Vector3 _movement = _direction * _normalSpeed * Time.fixedDeltaTime;
-        while(
-            Physics2D.Raycast(transform.position+new Vector3(0.5f,0.5f,0),_movement,_normalSpeed,_wallMask)||
-            Physics2D.Raycast(transform.position + new Vector3(0.5f,-0.5f,0),_movement,_normalSpeed,_wallMask) ||
-            Physics2D.Raycast(transform.position + new Vector3(-0.5f,-0.5f,0),_movement,_normalSpeed,_wallMask) ||
-            Physics2D.Raycast(transform.position + new Vector3(-0.5f,0.5f,0),_movement,_normalSpeed,_wallMask))
+        Vector3 _reversedMovement = _movement;
+        while(CantWalk(_movement)&&CantWalk(_reversedMovement))
         {
-            _movement = MathAVM.MathA.RotatedVector(_movement, 25);
-            if(_movement == _direction * _normalSpeed * Time.fixedDeltaTime) break;
+            _movement = MathAVM.MathA.RotatedVector(_movement,25);
+            _reversedMovement = MathAVM.MathA.RotatedVector(_reversedMovement,-25);
+            if(_movement == _direction * _normalSpeed * Time.fixedDeltaTime || !CantWalk(_movement)) break;
+            if(!CantWalk(_reversedMovement)) _movement = _reversedMovement;
         }
-        if(lastPos == transform.position.ConvertTo<Vector2>())
-        {
-            _movement = MathAVM.MathA.RotatedVector(_movement,180);
-        }
-        Debug.DrawRay(transform.position,_movement*22);
+        Debug.DrawRay(transform.position,_movement * 22);
         _rigidbody.MovePosition(transform.position + _movement);
         lastPos = transform.position;
+    }
+
+    private bool CantWalk(Vector3 _movement)
+    {
+        return (Physics2D.Raycast(transform.position + new Vector3(0.5f,0.5f,0),_movement,_normalSpeed,_wallMask) ||
+                    Physics2D.Raycast(transform.position + new Vector3(0.5f,-0.5f,0),_movement,_normalSpeed,_wallMask) ||
+                    Physics2D.Raycast(transform.position + new Vector3(-0.5f,-0.5f,0),_movement,_normalSpeed,_wallMask) ||
+                    Physics2D.Raycast(transform.position + new Vector3(-0.5f,0.5f,0),_movement,_normalSpeed,_wallMask))
+                    && !Physics2D.Raycast(transform.position,_movement,_normalSpeed,_playerMask);
     }
 
     protected override void CloseAttack(Collision2D other)
