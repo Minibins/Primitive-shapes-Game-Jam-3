16259f09d99484b53a92ec9ff56b90782c5478dd
Minibins <127485465+Minibins@users.noreply.github.com>
2024-01-01 22:28:26 +0100

    Android

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-
 using UnityEngine;
 
 public class InputController : MonoBehaviour
@@ -7,9 +6,19 @@ public class InputController : MonoBehaviour
     [SerializeField] private GameObject _gunSwipe;
     [SerializeField] private GameObject SettingsPanel;
     [SerializeField] private GameObject _statsPanel;
+
+    [SerializeField] private Joystick _moveJoystick;
+    [SerializeField] private Joystick _fireJoystick;
+
+    [SerializeField] private bool _isAndroid;
+    public static bool IssAndroid;
+
     private IShooting _shooting;
     public static float _horizontalInput;
     public static float _verticalInput;
+
+    public static float _horizontalJoystickInput;
+    public static float _verticalJoystickInput;
 
 
     private void Start()
@@ -19,39 +28,69 @@ public class InputController : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        IssAndroid = _isAndroid;
+
+        if (!_isAndroid)
         {
-            _shooting.SingleFire();
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                _shooting.SingleFire();
+            }
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Fire();
+            }
         }
-        if(Input.GetKey(KeyCode.Mouse0))
+        else if(_fireJoystick.Horizontal > 0 || _fireJoystick.Horizontal < 0 || _fireJoystick.Vertical > 0 || _fireJoystick.Vertical < 0)
         {
-            _shooting.Fire();
+            Fire();
         }
 
         if (Input.GetKeyDown(KeyCode.F3))
         {
             _statsPanel.SetActive(!_statsPanel.activeSelf);
         }
-        
+
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SettingsPanel.GetComponent<Settings>().PauseMenu(true);
+            OpenPauseMenu();
         }
-        
-         var _scrollWheel = Input.GetAxis("Mouse ScrollWheel");
-         
-         if (_scrollWheel > 0f)
-         {
-             _shooting.SwipeUpGun();
-         }
-         else if (_scrollWheel < 0f)
-         {
+
+        var _scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+
+        if (_scrollWheel > 0f)
+        {
+            _shooting.SwipeUpGun();
+        }
+        else if (_scrollWheel < 0f)
+        {
             _shooting.SwipeDownGun();
-         }
-         
-         
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _verticalInput = Input.GetAxis("Vertical");
+        }
+
+        if (_isAndroid)
+        {
+            _horizontalInput = _moveJoystick.Horizontal;
+            _verticalInput = _moveJoystick.Vertical;
+
+            _horizontalJoystickInput = _fireJoystick.Horizontal;
+            _verticalJoystickInput = _fireJoystick.Vertical;
+        }
+        else
+        {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _verticalInput = Input.GetAxis("Vertical");
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        SettingsPanel.GetComponent<Settings>().PauseMenu(true);
+    }
+
+    public void Fire()
+    {
+        _shooting.Fire();
     }
 }
